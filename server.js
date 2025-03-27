7267858d016191aa8d28580323f7f6822ad18cca
Huyhuynh2003 <huynhhuy2003h@gmail.com>
2025-03-27 15:38:49 +0700

    Remove index.js and update to use server.js

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Kết nối MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 mongoose.connection.on("connected", () => console.log("✅ Kết nối MongoDB thành công!"));
 mongoose.connection.on("error", (err) => console.error("❌ Lỗi MongoDB:", err));
